21d93cef0a4a2fee9b909ad263daf6d0ab78e4e2
mangopanda455 <gfielding26@scarsdaleschools.org>
2022-11-02 19:25:22 -0400

    edits to movementCollision.py and new file for ML

---
+++ MLhaha.py
@@ -0,0 +1,16 @@
+import vowpalwabbit
+
+model = vowpalwabbit.Workspace(quiet=True)
+train_examples = [
+    "0 | price:.23 sqft:.25 age:.05 2006",
+    "1 | price:.18 sqft:.15 age:.35 1976",
+    "0 | price:.53 sqft:.32 age:.87 1924",
+]
+
+for example in train_examples:
+    model.learn(example)
+
+test_example = "| price:.46 sqft:.4 age:.10 1924"
+
+prediction = model.predict(test_example)
+print(prediction)
--- movementCollision.py
+++ movementCollision.py
@@ -2,7 +2,7 @@ import pygame
 pygame.init()
 running = True
 screen = pygame.display.set_mode((600, 600))
-rect1 = pygame.Rect(*screen.get_rect().center, 0, 0).inflate(75, 75)
+rect1 = pygame.Rect(0, 0, 75, 75)
 color = (255, 0, 0)
 while running:
     for event in pygame.event.get():
@@ -11,6 +11,8 @@ while running:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
-
+    screen.fill(0)
+    rect1.center = pygame.mouse.get_pos()
     pygame.draw.rect(screen, color, rect1, 6, 1)
     pygame.display.update()
+    pygame.display.flip()
